a3a0441164228c41ee5fbf9918e88a15721a8f7d
Tokhirbek <aminjonovtoxirbek@gmail.com>
2024-11-14 00:30:09 +0900

    BBR: build express web server

---
+++ server.js
@@ -0,0 +1,34 @@
+console.log(' web Serverni boshlashlash')
+
+const express = require("express");
+const app = express();
+const http = require('http');
+
+// 1  Kirish codelari yoziladi:
+
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+
+// 2 Section qismi ikkinchi bo'limda yoziladi:
+
+
+// 3  Bu qismda tradional usulda backendni ichida frontend qismi yasaladi => Views ga bog'liq ko'dlar yoziladi:
+app.set('views', "views");
+app.set("view engine", 'esj')
+
+// 4 Bu qadam asosan routerlarga mo'ljallangan, ya'ni Routing codelar yoziladi:
+
+app.get('/hello', function(req, res) {
+    res.end(`<h1 style = "background: red"> You are in hello section</h1>`);
+});
+
+app.get('/gift', function(req, res) {
+    res.end(`<h1 style = "background: blue; color: white"> You are in gift section</h1>`);
+});
+const server = http.createServer(app);
+let PORT = 3000;
+server.listen(PORT, function(){
+    console.log(`The server is running seccesfully on port: ${PORT}`)
+});
