53945c0d5f8e3e2598ac72c21b4291974d917a1e
Tokhirbek <aminjonovtoxirbek@gmail.com>
2024-11-21 01:09:36 +0900

    TASK A is Solved By Jeyms

--- train.js
+++ train.js
@@ -16,12 +16,16 @@ function maslahatBering(a, callback) {
     else if(a > 20 && a <= 30 ) callback (null, list[2]);
     else if(a > 20 && a <= 30 ) callback (null, list[3]);
     else if(a > 20 && a <= 30 ) callback (null, list[4]);
-     else {
-          setImmediate(function () {
-                callback(null, list[5]);
-          }, 5000);
+                    else {
+                         setImmediate(function () {
+                              callback(null, list[5]);
+                         }, 100);
+                    }
+
+                    // else{
+                    //      callback(null, list[5]);
+                    // }
      }
-}
 
 console.log("Passed here 0");
 
@@ -33,3 +37,62 @@ maslahatBering(65, (err, data) => {
 });
 
 console.log("Passed here 1")
+
+
+
+
+
+
+/// TASK A 
+
+
+//  Masalani izohi: 
+
+/* 
+     Shunday 2 parametrli function tuzing, hamda birinchi parametrdagi letterni ikkinchi parametrdagi sozdan qatnashga sonini return qilishi kerak boladi.
+     MASALAN countLetter("e", "engineer") 3ni return qiladi.
+*/
+
+// SOLUTION:
+
+function harfSanash(harf, matn) {
+
+     if (typeof harf !== "string" || harf.length !== 1) {
+          console.log("Iltimos, faqat bitta harf kiriting!")
+          return ;
+      }
+      if (typeof matn !== "string" || matn.length === 0) {
+          console.log("Iltimos, so'z kiriting!")
+          return;
+      }
+
+     harf = harf.toLowerCase();
+     matn = matn.toLowerCase();
+     let count = 0; 
+     for (let i = 0; i < matn.length; i++) {
+         if (matn[i] === harf) {
+             console.log(matn[i])
+             count++;
+         }
+     }
+     // return count;
+     console.log("Siz kiritgan so'zda " + count + " ta birxil harf qatnashgan !"); 
+ }
+ 
+ 
+
+ harfSanash("ee", "enginer");
+
+ console.log("================================");
+ 
+ harfSanash("e", 1);
+
+ console.log("================================");
+
+ harfSanash("e", "EngineErINg");
+
+ console.log("================================");
+
+ harfSanash(2, "EngineErINg");
+
+ console.log("================================")
