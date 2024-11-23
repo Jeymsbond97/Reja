3052e1fe0f7d15de828f28da0cd48cf0177c5db4
Tokhirbek <aminjonovtoxirbek@gmail.com>
2024-11-23 21:55:58 +0900

    Errors are fixed

--- app.js
+++ app.js
@@ -1,91 +1,146 @@
-console.log(' web Serverni boshlashlash')
+// console.log(' web Serverni boshlashlash')
 
-const express = require("express");
-const res = require('express/lib/response')
-const app = express();
+// const express = require("express");
+// const res = require('express/lib/response')
+// const app = express();
 
-// MongoDB ni chaqirish: 
+// // MongoDB ni chaqirish: 
 
-const db = require("./server").db();
+// const db = require("./server").db();
 
-// const fs = require('fs');
+// // const fs = require('fs');
 
 
-// let user;
-// fs.readFile("database/user.json", "utf-8", (err, data)=>{
-//     if(err){
-//         console.log("Error:", err);
-//     }
-//     else{
-//         user = JSON.parse(data);
-//     }
+// // let user;
+// // fs.readFile("database/user.json", "utf-8", (err, data)=>{
+// //     if(err){
+// //         console.log("Error:", err);
+// //     }
+// //     else{
+// //         user = JSON.parse(data);
+// //     }
+// // })
+
+// /*  Server yaratishni ikki hil usuli bor : 
+//   1. Traditional method yani bu biz buni express package yordami ejs orqali yaratdik.
+//   2. Single page orqali yani bunda biz reac orqali yaratishimiz mumkun!
+
+// */
+
+
+
+// // 1  Kirish codelari yoziladi:
+
+// app.use(express.static('public'));
+// app.use(express.json());
+// app.use(express.urlencoded({extended: true}));
+
+
+// // 2 Section qismi ikkinchi bo'limda yoziladi:
+
+
+// // 3  Bu qismda tradional usulda backendni ichida frontend qismi yasaladi => Views ga bog'liq ko'dlar yoziladi:
+// app.set('views', "views");
+// app.set("view engine", 'ejs'); 
+
+// // 4 Bu qadam asosan routerlarga mo'ljallangan, ya'ni Routing codelar yoziladi:
+// app.post('/create-item', (req, res)=>{
+//     console.log("user entered /create-item");  
+//     const new_reja = req.body.reja;
+//     db.collection("plans").insertOne({reja: new_reja}, (err, data) => {
+//         if(err) {
+//             console.log(err);
+//             res.end("something went wrong");
+//         }else{
+//             // console.log(data);
+//             res.end("Successfully added")
+//         }
+//     });
+    
+//     /// Post -> bu o'zi bilan malumot olib keladi va uni database ga yozish uchun xizmat qiladi:
+//     // res.json({test: "succes"});
 // })
 
-/*  Server yaratishni ikki hil usuli bor : 
-  1. Traditional method yani bu biz buni express package yordami ejs orqali yaratdik.
-  2. Single page orqali yani bunda biz reac orqali yaratishimiz mumkun!
+// app.get('/author', (req, res)=> {
+//     res.render('author', {user: user} )
+// })
 
-*/
+// app.get('/', function (req, res) {
+//     console.log("user entered /");
+//     db.collection("plans").find().toArray((err, data) =>
+//     {
+//         if(err){
+//             console.log(err);
+//             res.end('something went wrong')
+//         } else{
+//             res.render('reja', {items: data });
+//         }
+//     })
+//     // res.render('reja');   /// get -> Bu esa database dan malumot olish vao'qish uchun ishlatiladi:
+// });
+
+// // app.get('/hello', function(req, res) {
+// //     res.end(`<h1 style = "background: red"> You are in hello section</h1>`);
+// // });
+
+// app.get('/gift', function(req, res) {
+//     res.end(`<h1 style = "background: blue; color: white"> You are in gift section</h1>`);
+// });
 
 
+// module.exports = app;
 
-// 1  Kirish codelari yoziladi:
 
-app.use(express.static('public'));
+console.log('Web Serverni boshlash');
+const express = require("express"); 
+const app = express(); 
+
+//MongoDB chaqirish
+const db = require("./server").db();
+
+// *****1 - Kirish codelari*******
+app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+// ********2 - Sessionga bogliq bolgan codelar*******
+// ********3 - Views ga bogliq codelar*********
 
-// 2 Section qismi ikkinchi bo'limda yoziladi:
+app.set("views", "views");
+app.set("view engine", "ejs"); 
 
-
-// 3  Bu qismda tradional usulda backendni ichida frontend qismi yasaladi => Views ga bog'liq ko'dlar yoziladi:
-app.set('views', "views");
-app.set("view engine", 'ejs'); 
-
-// 4 Bu qadam asosan routerlarga mo'ljallangan, ya'ni Routing codelar yoziladi:
-app.post('/create-item', (req, res)=>{
-    console.log("user entered /create-item");  
+// *****4 - Rooting ga bogliq codelar********
+app.post("/create-item", (req, res) => {
+    console.log('user entered /create-item');
+    console.log(req.body);
+    // res.end("success");
     const new_reja = req.body.reja;
     db.collection("plans").insertOne({reja: new_reja}, (err, data) => {
         if(err) {
             console.log(err);
-            res.end("something went wrong");
-        }else{
-            // console.log(data);
+            res.end("Something went wrong")
+        } else {
+            console.log(new_reja);
             res.end("Successfully added")
         }
-    });
-    
-    /// Post -> bu o'zi bilan malumot olib keladi va uni database ga yozish uchun xizmat qiladi:
-    // res.json({test: "succes"});
-})
-
-app.get('/author', (req, res)=> {
-    res.render('author', {user: user} )
-})
-
-app.get('/', function (req, res) {
-    console.log("user entered /");
-    db.collection("plans").find().toArray((err, data) =>
-    {
-        if(err){
-            console.log(err);
-            res.end('something went wrong')
-        } else{
-            res.render('reja', {items: data });
-        }
     })
-    // res.render('reja');   /// get -> Bu esa database dan malumot olish vao'qish uchun ishlatiladi:
-});
+})
 
-// app.get('/hello', function(req, res) {
-//     res.end(`<h1 style = "background: red"> You are in hello section</h1>`);
-// });
-
-app.get('/gift', function(req, res) {
-    res.end(`<h1 style = "background: blue; color: white"> You are in gift section</h1>`);
-});
+app.get("/", function(req, res) {
+    console.log('user entered /');
+    db.collection("plans")
+    .find()
+    .toArray((err, data) => {
+        if(err) {
+            console.log(err);
+            res.end("something went wrong");
+        } else {
+            // console.log(data);
+            res.render("reja", { items: data });
+        }
+    });
+})
 
 
 module.exports = app;
+
