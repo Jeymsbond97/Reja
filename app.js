7261eee31faa536dfe867d21483ca1b3707c2997
Tokhirbek <aminjonovtoxirbek@gmail.com>
2024-11-23 19:20:49 +0900

    feat: CRUD is fixed

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.set("view engine", 'ejs');
 
 // 4 Bu qadam asosan routerlarga mo'ljallangan, ya'ni Routing codelar yoziladi:
 app.post('/create-item', (req, res)=>{
-    console.log(req.body);  
+    console.log("user entered /create-item");  
     const new_reja = req.body.reja;
     db.collection("plans").insertOne({reja: new_reja}, (err, data) => {
         if(err) {
@@ -66,13 +66,13 @@ app.get('/author', (req, res)=> {
 })
 
 app.get('/', function (req, res) {
+    console.log("user entered /");
     db.collection("plans").find().toArray((err, data) =>
     {
         if(err){
             console.log(err);
             res.end('something went wrong')
         } else{
-            console.log(data);
             res.render('reja', {items: data });
         }
     })
